1e232f25a37a20c227e5f46dfa138e29a822aff5
Muhammad Azam <AzamWahid@outlook.com>
2025-01-23 20:22:52 +0500

    styling added

--- app.js
+++ app.js
@@ -21,17 +21,25 @@ fetch('https://restcountries.com/v3.1/all').then((countryAllData) => {
     if (pakistanOption) {
         pakistanOption.selected = true;
     }
-    dropDown.dispatchEvent(new MouseEvent('onchange'));
+    dropDown.dispatchEvent(new MouseEvent('change'));
 })
 
-dropDown.addEventListener('onchange', () => {
+dropDown.addEventListener('change', () => {
     console.log("hwllo");
     specificCntry = fetch(`https://restcountries.com/v3.1/name/${dropDown.value}`).then((countrySepcficData) => {
         return countrySepcficData.json();
     }).then((data) => {
+        const currencyCode = Object.keys(data[0].currencies)[0]; // Gets the first currency code
+        const currency = data[0].currencies[currencyCode];
+        console.log(currency)
         cardImg.src = data[0].flags?.png;
         cardTitle.innerHTML = data[0].name?.common;
-        cardText.innerHTML = `Capital of ${data[0].name.common} : ${data[0].capital[0]}`
+        cardText.innerHTML = `${data[0].name.common} (${data[0].name.official}) is located in the ${data[0].region} region, specifically in ${data[0].subregion}. ` +
+                            `Its capital city is ${data[0].capital}, and it has a population of approximately ${data[0].population.toLocaleString()} people. ` +
+                            `The country covers an area of ${data[0].area.toLocaleString()} square kilometers. ` +
+                            `It shares borders with ${data[0].borders.join(", ")}. ` +
+                            `${data[0].name.common} uses the ${currency.name} (${currency.symbol}) as its currency. ` +
+                            `The flag of France is represented as ${data[0].flags.alt}.`
     })
 })
 
